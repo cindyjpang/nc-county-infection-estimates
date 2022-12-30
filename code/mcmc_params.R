91559a79e900940ae9fae11ca88ccf4b2742af1d
Cindy Pang <77863668+cindyjpang@users.noreply.github.com>
2022-12-30 13:39:08 -0500

    clean outputs

---
+++ code/mcmc_params.R
@@ -0,0 +1,19 @@
+## ---------------------------
+##
+## Script name: mcmc_params.R
+##
+## Purpose of script: using the results in COVID-19 Forecasting Team et al. 2021, display parameters for MCMC simulation 
+##
+## Author: Cindy J. Pang
+##
+## Date Created: 2022-12-30
+##
+## Copyright (c) Cindy Pang, 2022
+## Email: pangcind@live.unc.edu
+##
+## ---------------------------
+##
+## Notes:
+##   
+##
+## ---------------------------
